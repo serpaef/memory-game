db955c0c037df7a43ea7bcfea9d5ca160b2ec826
Fernando Serpa <serpaef@outlook.com>
2022-06-16 16:17:15 -0300

    feat: first steps on game logic

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
   this.classList.add('flip');
+  
+  if(!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkMatching();
+}
+
+function checkMatching() {
+  if(firstCard.dataset.card === secondCard.dataset.card) {
+    // TODO disableCards();
+    return;
+  }
+
+  // TODO unflipCards();
 }
 
 cards.forEach((card) => {
   card.addEventListener('click', flipCard);
 });
-
